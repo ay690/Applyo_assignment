6d814ede8fd30d13d8f4b4ee63855ce9a6d6e1ab
Aniket Yadav <aniketyadav690@gmail.com>
2025-08-05 18:10:04 +0530

    update: movie grid spacing

--- src/components/movie-grid.tsx
+++ src/components/movie-grid.tsx
@@ -49,7 +49,7 @@ export default function MovieGrid() {
 
   return (
     <>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 md:gap-6">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 md:gap-6 p-3 lg:p-20">
         {movies.map((movie) => (
           <MovieCard
             key={movie.imdbID}
